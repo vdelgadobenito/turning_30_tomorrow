1bdb67137394ea5e4cf661212b5e6fd758edc42b
Veronica Delgado <veronica.delgado58@gmail.com>
2021-02-22 16:00:28 +0100

    lib file

---
+++ turning_30_tomorrow/lib.py
@@ -0,0 +1,5 @@
+def try_me(your_age):
+    if your_age < 30:
+        print('The truth is of course is that there is no journey. We are arriving and departing all at the same time. David Bowie')
+    else:
+        print('You got to get it while you can. Janis Joplin')
